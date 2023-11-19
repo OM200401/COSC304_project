b9999749069bbfa6269ca721c6e529e4f92f7c93
aamirahmed2004 <syedaamir099@gmail.com>
2023-11-18 22:24:23 -0800

    auth not working

--- routes/admin.js
+++ routes/admin.js
@@ -1,22 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const moment = require('moment');
 const auth = require('../auth');
 const sql = require('mssql');
 
 router.get('/', function(req, res, next) {
-
-	
-	// TODO: Include files auth.jsp and jdbc.jsp
-	
-	
-	
+    
     res.setHeader('Content-Type', 'text/html');
 
     (async function() {
         try {
+            console.log("hello")
+            res.write('<h3>Admin Sales Report by Day</h3>');
+
             let pool = await sql.connect(dbConfig);
 
-	    // TODO: Write SQL query that prints out total order amount by day
+            let sqlQuery = "SELECT CONVERT(date, orderDate) AS orderDay, sum(totalAmount) as total FROM orderSummary GROUP BY CONVERT(date,orderDate)";
+            
+            let results = await pool.request().query(sqlQuery);
+
+            res.write("<table class=\"table\" border=\"1\"><tr><th>Order Date</th><th> Total Order Amount</th></tr>");
+
+            for (let i = 0; i < results.recordset.length; i++) {
+                let result = results.recordset[i];
+                console.log(result);
+
+                res.write("</td><td>" + moment(result.orderDay).format("YYYY-MM-DD") + 
+                          "</td><td>" + "$" + result.total.toFixed(2) + "</td></tr>");
+            }
+            res.write("</table>");
+            res.end();
         } catch(err) {
             console.dir(err);
             res.write(err + "");
